fe2a193f3ce1807959457dc92d1098292003a610
simon <tran.simon2@gmail.com>
2022-12-20 19:54:04 -0500

    Add sakila installation script

--- README.md
+++ README.md
@@ -18,8 +18,77 @@ chmod 400 final-project.pem
 
 # Installation
 
-1. Install MySQL on the standalone VM
-2. Install MySQL Cluster on the master VM:
+### Run the setup script for the standalone VM:
+
 ```bash
+# Connect to the VM via SSH
+./script.sh -c
+
+# Install mysql-server
+apt-get -y install mysql-server
+```
+
+Then follow the [instructions](https://www.linode.com/docs/guides/install-mysql-on-ubuntu-14-04/) to complete the MySQL installation.
+
+Then install [Sakila](#sakila)
+
+### Install MySQL Cluster on the master VM:
+
+```bash
+# Install the dependencies
+./script.sh -d master
+
+# Complete MySQL Cluster installation 
 ./script.sh -i master
 ```
+
+### Install MySQL Cluster on the slave VMs:
+
+```bash
+# Install the dependencies
+./script.sh -d slave1
+./script.sh -d slave2
+./script.sh -d slave3
+
+# Complete MySQL Cluster installation
+./script.sh -i slave1
+./script.sh -i slave2
+./script.sh -i slave3
+```
+
+### Execute the final setup script for the master VM:
+
+```bash
+./script.sh -f
+```
+
+Then, you need to secure the MySQL installation
+
+```bash
+# Connect to the master VM via SSH
+./script.sh -c master
+
+mysql_secure_installation
+```
+
+## Sakila
+
+You need to install Sakila on the standalone and the master VM
+
+```bash
+./script.sh -s standalone
+```
+
+Connect to the VM and populate the DB
+
+```bash
+./script.sh -c standalone
+
+mysql -u root -p
+
+SOURCE sakila-db/sakila-schema.sql;
+SOURCE sakila-db/sakila-data.sql;
+exit;
+```
+
+Repeat the steps for the master VM
--- scripts.sh
+++ scripts.sh
@@ -5,11 +5,11 @@ set -e
 username="ubuntu"
 pem_path="final-project.pem"
 
-standalone_ip="52.90.91.135"
-master_ip="23.23.38.137"
-slave1_ip="34.228.55.223"
-slave2_ip="54.209.182.210"
-slave3_ip="34.229.81.60"
+standalone_ip="44.201.156.178"
+master_ip="54.91.243.233"
+slave1_ip="54.226.24.9"
+slave2_ip="3.91.46.155"
+slave3_ip="100.26.48.3"
 master_ip_internal="ip-172-31-17-89.ec2.internal"
 slave1_ip_internal="ip-172-31-21-198.ec2.internal"
 slave2_ip_internal="ip-172-31-18-51.ec2.internal"
@@ -49,25 +49,26 @@ copy_to_vm() {
   scp -i "$pem_path" "$1" "$username"@"$vm_address":"$2"
 }
 
+install_mysql_cluster_dependencies() {
+    copy_to_vm "resources/mysqlc.sh" "mysqlc.sh"
+    run_on_vm "mkdir -p /opt/mysqlcluster/home && \
+      cd /opt/mysqlcluster/home && \
+      wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz && \
+      tar xvf mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz && \
+      ln -sf mysql-cluster-gpl-7.2.1-linux2.6-x86_64 mysqlc && \
+      rm -rf mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz && \
+      mv /home/ubuntu/mysqlc.sh /etc/profile.d/mysqlc.sh
+      "
+
+    echo "2. Install libncurses5"
+    run_on_vm "apt-get update && \
+      apt-get -y install libncurses5
+      "
+}
+
 # https://stansantiago.wordpress.com/2012/01/04/installing-mysql-cluster-on-ec2/
-function install_mysql_cluster() {
+install_mysql_cluster() {
   echo "Installing MySQL Cluster on $vm_name"
-
-  echo "1. Common Steps for all Nodes"
-  copy_to_vm "resources/mysqlc.sh" "mysqlc.sh"
-  run_on_vm "mkdir -p /opt/mysqlcluster/home && \
-    cd /opt/mysqlcluster/home && \
-    wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz && \
-    tar xvf mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz && \
-    ln -sf mysql-cluster-gpl-7.2.1-linux2.6-x86_64 mysqlc && \
-    rm -rf mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz && \
-    mv /home/ubuntu/mysqlc.sh /etc/profile.d/mysqlc.sh
-    "
-
-  echo "2. Install libncurses5"
-  run_on_vm "apt-get update && \
-    apt-get -y install libncurses5
-    "
 
   if [ "$vm_name" = "master" ]; then
     echo "----- SQL/Mgmt Node specific steps -----"
@@ -111,7 +112,7 @@ nodeid=50"
       mkdir -p mysqld_data && \
       mkdir -p ndb_data && \
       cd conf && \
-      echo -e '$my_cfg' > my.cfg && \
+      echo -e '$my_cfg' > my.cnf && \
       echo -e '$config_ini' > config.ini
       "
 
@@ -133,17 +134,35 @@ nodeid=50"
   fi
 }
 
-while getopts 'c:i:m' flag; do
+install_sakila() {
+  run_on_vm "wget https://downloads.mysql.com/docs/sakila-db.tar.gz && \
+    tar xvf sakila-db.tar.gz
+    "
+}
+
+while getopts 'c:d:i:s:fm' flag; do
   case "${flag}" in
   c)
     # Connect to the VM in ssh
     set_vm_address "${OPTARG}"
     run_on_vm ""
     ;;
+  d)
+    # Install dependencies on the VM
+    set_vm_address "${OPTARG}"
+    install_mysql_cluster_dependencies
+    ;;
   i)
     # Install MySQL Cluster
     set_vm_address "${OPTARG}"
     install_mysql_cluster
+    ;;
+  f)
+    # Final setup steps for master
+    set_vm_address master
+    run_on_vm "source /etc/profile.d/mysqlc.sh  && \
+      mysqld --defaults-file=/opt/mysqlcluster/deploy/conf/my.cnf --user=root
+      "
     ;;
   m)
     # Run `ndb_mgm` on the master
@@ -151,6 +170,11 @@ while getopts 'c:i:m' flag; do
     run_on_vm "source /etc/profile.d/mysqlc.sh  && \
       ndb_mgm
       "
+    ;;
+  s)
+    # Install Sakila on the VM
+    set_vm_address "${OPTARG}"
+    install_sakila
     ;;
   *) ;;
   esac
